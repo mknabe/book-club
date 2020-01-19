13b56c65667510f1038578f8d9ebde9d3bd7b832
Maria K <mknabe@credera.com>
2020-01-18 22:51:01 -0600

    Updates

---
+++ precache-manifest.7469287e93a2a262e47cf09b3d9bcc81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b92a1bd99408aefee54e263c04b9b1",
+    "url": "/book-club/index.html"
+  },
+  {
+    "revision": "dea9056409ffb617c4b1",
+    "url": "/book-club/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "265cde4944027266734a",
+    "url": "/book-club/static/css/main.10a59bcd.chunk.css"
+  },
+  {
+    "revision": "dea9056409ffb617c4b1",
+    "url": "/book-club/static/js/2.16d858c9.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/book-club/static/js/2.16d858c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "265cde4944027266734a",
+    "url": "/book-club/static/js/main.f5f94061.chunk.js"
+  },
+  {
+    "revision": "d3c01510082861c25fde",
+    "url": "/book-club/static/js/runtime-main.3ba1b1b7.js"
+  }
+]);
